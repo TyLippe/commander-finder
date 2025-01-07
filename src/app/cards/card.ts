f972b0fc67c9b3c13e1b6d59573831f4a76fdee3
Ty Lippe <tylippe94@gmail.com>
2025-01-07 10:15:07 -0500

    Quick styling

--- src/app/cards/card.ts
+++ src/app/cards/card.ts
@@ -1,4 +1,6 @@
 export interface Card {
   name: string;
   image_url: string;
+  set_name: string;
+  description: string;
 }
--- src/app/cards/cards-list/cards-list.component.ts
+++ src/app/cards/cards-list/cards-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { CardsService } from '../cards.service';
 import { Card } from '../card';
 
 @Component({
   selector: 'app-cards-list',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './cards-list.component.html',
   styleUrl: './cards-list.component.css',
 })
@@ -19,12 +20,16 @@ export class CardsListComponent implements OnInit {
 
   ngOnInit(): void {
     this.cardsService.getCommanders().subscribe((cards) => {
-      this.cards = cards.data.map((card: any) => {
-        return {
-          name: card.name,
-          image_url: card?.image_uris?.png || '',
-        };
-      });
+      this.cards = cards.data
+        .filter((card: any) => card?.image_uris?.png)
+        .map((card: any) => {
+          return {
+            name: card.name,
+            image_url: card?.image_uris?.png || '',
+            set_name: card.set_name,
+            description: card.oracle_text,
+          };
+        });
       this.next_page = cards.next_page;
     });
   }
@@ -33,12 +38,16 @@ export class CardsListComponent implements OnInit {
     if (this.next_page) {
       this.cardsService.getCommanders(this.next_page).subscribe((cards) => {
         this.cards = this.cards.concat(
-          cards.data.map((card: any) => {
-            return {
-              name: card.name,
-              image_url: card?.image_uris?.png || '',
-            };
-          })
+          cards.data
+            .filter((card: any) => card?.image_uris?.png)
+            .map((card: any) => {
+              return {
+                name: card.name,
+                image_url: card?.image_uris?.png || '',
+                set_name: card.set_name,
+                description: card.oracle_text,
+              };
+            })
         );
         this.next_page = cards.next_page;
       });
