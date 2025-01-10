977fae5f0d2f9ba2ecce70fcebb87d4aa760ed73
Ty Lippe <tylippe94@gmail.com>
2025-01-10 14:25:10 -0500

    Added a filter bar for cosmetics / Added flipped card logic

--- src/app/cards/card.ts
+++ src/app/cards/card.ts
@@ -3,4 +3,8 @@ export interface Card {
   image_url: string;
   set_name: string;
   description: string;
+  canBeFlipped: boolean;
+  alt_image_url?: string;
+  alt_description?: string;
+  alt_name?: string;
 }
--- src/app/cards/cards-list/cards-list.component.ts
+++ src/app/cards/cards-list/cards-list.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
-import { CardsService } from '../cards.service';
 import { Card } from '../card';
+import { CardsService } from '../cards.service';
+import { FilterBarComponent } from '../filter-bar/filter-bar.component';
 
 @Component({
   selector: 'app-cards-list',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, FilterBarComponent, CommonModule],
   templateUrl: './cards-list.component.html',
   styleUrl: './cards-list.component.css',
 })
@@ -20,16 +22,9 @@ export class CardsListComponent implements OnInit {
 
   ngOnInit(): void {
     this.cardsService.getCommanders().subscribe((cards) => {
-      this.cards = cards.data
-        .filter((card: any) => card?.image_uris?.png)
-        .map((card: any) => {
-          return {
-            name: card.name,
-            image_url: card?.image_uris?.png || '',
-            set_name: card.set_name,
-            description: card.oracle_text,
-          };
-        });
+      this.cards = cards.data.map((card: any) => {
+        return this.shapeCardData(card);
+      });
       this.next_page = cards.next_page;
     });
   }
@@ -38,19 +33,44 @@ export class CardsListComponent implements OnInit {
     if (this.next_page) {
       this.cardsService.getCommanders(this.next_page).subscribe((cards) => {
         this.cards = this.cards.concat(
-          cards.data
-            .filter((card: any) => card?.image_uris?.png)
-            .map((card: any) => {
-              return {
-                name: card.name,
-                image_url: card?.image_uris?.png || '',
-                set_name: card.set_name,
-                description: card.oracle_text,
-              };
-            })
+          cards.data.map((card: any) => {
+            return this.shapeCardData(card);
+          })
         );
         this.next_page = cards.next_page;
       });
+    }
+  }
+
+  shapeCardData(card: any): Card {
+    return {
+      name: card?.card_faces?.length ? card?.card_faces[0]?.name : card.name,
+      image_url:
+        card?.image_uris?.png || card?.card_faces[0]?.image_uris?.png || '',
+      set_name: card.set_name,
+      description: card.oracle_text || card.card_faces[0]?.oracle_text,
+      canBeFlipped: card.card_faces ? true : false,
+      alt_image_url: card?.card_faces?.length
+        ? card?.card_faces[1]?.image_uris?.png
+        : '',
+      alt_description: card?.card_faces?.length
+        ? card?.card_faces[1]?.oracle_text
+        : '',
+      alt_name: card?.card_faces?.length ? card?.card_faces[1]?.name : '',
+    };
+  }
+
+  flipCard(card: Card): void {
+    if (card.canBeFlipped) {
+      const tempImage = card.image_url;
+      const tempDescription = card.description;
+      const tempName = card.name;
+      card.image_url = card.alt_image_url || '';
+      card.description = card.alt_description || '';
+      card.name = card.alt_name || '';
+      card.alt_image_url = tempImage;
+      card.alt_description = tempDescription;
+      card.alt_name = tempName;
     }
   }
 }
